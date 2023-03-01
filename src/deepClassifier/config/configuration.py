f2806484caf026258d1d4f0f36357af59ceea106
ArunKumar237 <arunchary237@gmail.com>
2023-03-01 13:29:41 +0530

    Stage 03 training completed succesfully (after small correction)

--- src/deepClassifier/config/configuration.py
+++ src/deepClassifier/config/configuration.py
@@ -49,24 +49,6 @@ class ConfigurationManager:
 
         return prepare_base_model_config
 
-    def get_prepare_base_model_config(self) -> PrepareBaseModelConfig:
-        config = self.config.prepare_base_model
-        
-        create_directories([config.root_dir])
-
-        prepare_base_model_config = PrepareBaseModelConfig(
-            root_dir=Path(config.root_dir),
-            base_model_path=Path(config.base_model_path),
-            updated_base_model_path=Path(config.updated_base_model_path),
-            params_image_size=self.params.IMAGE_SIZE,
-            params_learning_rate=self.params.LEARNING_RATE,
-            params_include_top=self.params.INCLUDE_TOP,
-            params_weights=self.params.WEIGHTS,
-            params_classes=self.params.CLASSES
-        )
-
-        return prepare_base_model_config
-
     def get_prepare_callback_config(self) -> PrepareCallbacksConfig:
         config = self.config.prepare_callbacks
         model_ckpt_dir = os.path.dirname(config.checkpoint_model_filepath)
