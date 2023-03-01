a5f0c8d95cfe05e9bf969c1c5183a6a616217723
ArunKumar237 <arunchary237@gmail.com>
2023-03-01 12:54:05 +0530

    Prepare callbacks completed

--- src/deepClassifier/config/configuration.py
+++ src/deepClassifier/config/configuration.py
@@ -1,7 +1,8 @@
 from deepClassifier.constants import CONFIG_FILE_PATH, PARAMS_FILE_PATH
 from deepClassifier.utils import read_yaml, create_directories, get_size
 from deepClassifier.entity.config_entity import (DataIngestionConfig,
-                                                PrepareBaseModelConfig)
+                                                PrepareBaseModelConfig,
+                                                PrepareCallbacksConfig)
 from pathlib import Path
 import os
 
@@ -47,4 +48,21 @@ class ConfigurationManager:
 
         return prepare_base_model_config
 
+    def get_prepare_base_model_config(self) -> PrepareBaseModelConfig:
+        config = self.config.prepare_base_model
+        
+        create_directories([config.root_dir])
+
+        prepare_base_model_config = PrepareBaseModelConfig(
+            root_dir=Path(config.root_dir),
+            base_model_path=Path(config.base_model_path),
+            updated_base_model_path=Path(config.updated_base_model_path),
+            params_image_size=self.params.IMAGE_SIZE,
+            params_learning_rate=self.params.LEARNING_RATE,
+            params_include_top=self.params.INCLUDE_TOP,
+            params_weights=self.params.WEIGHTS,
+            params_classes=self.params.CLASSES
+        )
+
+        return prepare_base_model_config
     
